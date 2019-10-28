e881cb396ebe57dee3cffea17d92411d06d73558
Andrei Kassiantchouk <akass1122@gmail.com>
2019-10-28 15:38:37 -0700

    Cleaned up

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,25 @@
 import React, { Fragment, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
-import Routes from './components/routing/Routes';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Navbar from './components/layout/Navbar';
+// import Landing from './components/layout/Landing';
+// import Routes from './components/routing/Routes';
+import MainPage from './components/MainPage';
 
 // Redux
-import { Provider } from 'react-redux';
-import store from './store';
-import { loadUser } from './actions/auth';
-import setAuthToken from './utils/setAuthToken';
+// import { Provider } from 'react-redux';
+// import store from './store';
+// import { loadUser } from './actions/auth';
+// import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
-if (localStorage.token) {
-  setAuthToken(localStorage.token);
-}
+// if (localStorage.token) {
+//   setAuthToken(localStorage.token);
+// }
 
 const App = () => {
-  useEffect(() => {
-    store.dispatch(loadUser());
-  }, []);
-
   return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <Switch>
-            <Route exact path='/' component={Landing} />
-            <Route component={Routes} />
-          </Switch>
-        </Fragment>
-      </Router>
-    </Provider>
+    <MainPage />
   );
 };
 
