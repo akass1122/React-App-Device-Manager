3e5f62c4d9f757c25992ae685629ac20ee23e0b6
Andrei Kassiantchouk <akass1122@gmail.com>
2019-10-28 23:53:31 -0700

    Done

---
+++ client/build/precache-manifest.10d7a4286f0b00e31487ff0e90b302e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f80a70f49cafc0daaf51a17be75437eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cbc91321e8ee989bd52",
+    "url": "/static/css/main.ede1c1f7.chunk.css"
+  },
+  {
+    "revision": "33e3a6ea28ccd33c5d2a",
+    "url": "/static/js/2.1467d4f2.chunk.js"
+  },
+  {
+    "revision": "6cbc91321e8ee989bd52",
+    "url": "/static/js/main.b70fcfe8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
